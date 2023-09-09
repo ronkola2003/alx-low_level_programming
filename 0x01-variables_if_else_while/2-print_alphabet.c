d676526e65423893674f4a30eb13073c16682847
ronkola2003 <oyebeautypalace@gmail.com>
2023-09-08 17:17:10 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return : 0
+ */
+
+int main(void)
+
+{
+char j;
+
+for (j = 'a', j <= 'z'; j++ ;)
+'putcher'(j);
+'putcher'('\n');
+
+return (0);
+
+}
