a2d9b3c0166eb80ec84945f791eef69664eb6e5a
ronkola2003 <oyebeautypalace@gmail.com>
2023-09-12 07:21:27 -0700

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,19 +5,21 @@
  *
  * Return: void
  */
+
 void print_alphabet_x10(void)
 {
-	char ch;
-	int i;
-	i = 0;
-	while (i < 10)
-	{
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-	_putchar(ch);
-	}
-	_putchar("\n");
-	i++;
-	}
+
+char ch;
+int i;
+i = 0;
+while (i < 10)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar("\n");
+i++;
+}
 
 }
