6677d7ffafc7787f36cedfb4bd8c933dd844caad
ronkola2003 <oyebeautypalace@gmail.com>
2023-09-12 16:14:33 -0700

    updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,17 +7,17 @@
  */
 int print_last_digit(int n)
 {
-  int num;
+int num;
 
-  if (n < 0)
-    n = -n;
+if (n < 0)
+n = -n;
 
-  num = n % 10;
+num = n % 10;
 
-  if (num < 0)
-    num = -num;
+if (num < 0)
+num = -num;
 
-  _putchar(num + '0');
+_putchar(num + '0');
 
-  return (num);
+return (num);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,29 +5,29 @@
  */
 void times_table(void)
 {
-  int x, y, k;
+int x, y, k;
 
-  for (x = 0; x < 10; x++)
-    {
-      for (y = 0; y < 10; y++)
-	{
-	  k = x * y;
-	  if (y == 0)
-	    _putchar(k + '0');
-	  if (y != 0 && k < 10)
-	    {
-	      _putchar(',');
-	      _putchar(' ');
-	      _putchar(' ');
-	      _putchar(k + '0');
-	    } else if (k >= 10)
-	    {
-	      _putchar(',');
-	      _putchar(' ');
-	      _putchar((k / 10) + '0');
-	      _putchar((k % 10) + '0');
-	    }
-	}
-      _putchar('\n');
-    }
+for (x = 0; x < 10; x++)
+{
+for (y = 0; y < 10; y++)
+{
+k = x * y;
+if (y == 0)
+_putchar(k + '0');
+if (y != 0 && k < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+} else if (k >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+}
+_putchar('\n');
+}
 }
