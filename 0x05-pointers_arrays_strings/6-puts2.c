9c8043059e0c23ca3963c79b3601b3eb211e6e6b
ronkola2003 <oyebeautypalace@gmail.com>
2023-09-18 11:56:38 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,24 +8,12 @@
  */
 void puts2(char *str)
 {
+int i;
 
-int longi = 0;
-int t = 0;
-char *y = str;
-int o;
-while (*y != '\0')
+for (i = 0; str[i] != '\n')
 {
-y++;
-longi++;
+if (i % 2 ==)
+	_putchar(str[i]);
 }
-t = longi - 1;
-for (o = 0 ; o <= t ; o++)
-{
-if (o % 2 == 0)
-{
-_putchar(str[0]);
-}
-_putchar('\n');
-}
-
+	_putchar('\n');
 }
