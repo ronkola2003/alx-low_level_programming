5ea15e92d37bc3308f867d8b1e0ccd13eeb27905
ronkola2003 <oyebeautypalace@gmail.com>
2023-09-18 12:51:59 -0700

    7-puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+void puts_half(char *str)
+{
+int longi = 0;
+
+while (*s != '\0')
+{
+longi++;
+s++;
+}
+
+return (longi);
+}
