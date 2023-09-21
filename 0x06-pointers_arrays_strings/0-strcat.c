3ff1d33ecafba8e538e4e4713666c6991f282f05
ronkola2003 <oyebeautypalace@gmail.com>
2023-09-21 06:20:46 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,26 +2,25 @@
 
 /**
  * _strcat - concatenate two strings
- * @dest: parameter one
- * @src: parameter two
+ * @dest: parameter 1
+ * @src: parameter 2
  * Return: dest
  */
 
 char *_strcat(char *dest, char *src);
- {
+{
 int dest = i;
 int src = j;
 
 i = 0;
 while (dest[i] != '\0'; i++)
 	dest++;
-
+{
 j = 0;
 while (src[j] != '\0'; i++)
 	src++;
-{
-	dest[i] = src[j]
-		i++
 }
-return (dest);
+dest[i] = src[j]
+i++;
 }
+
