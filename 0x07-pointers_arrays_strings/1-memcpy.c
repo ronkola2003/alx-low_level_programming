71fafe0a648446fcade81950be40d1c9dfd3714f
ronkola2003 <oyebeautypalace@gmail.com>
2023-09-25 11:13:20 -0700

    1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,17 +5,16 @@
  * @dest: memory where is stored
  * @src: memory where is copied
  * *@n: number of bytes
+ *
  * Return: a pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n);
 {
-int r = 0;
-int i = n;
+unsigned int i;
 
-for (; r < i; r++)
+for (i = 0; i < n; i++)
 {
-dest[r] = src[r];
-n--;
+dest[i] = src[i];
 }
 return (dest);
 }
