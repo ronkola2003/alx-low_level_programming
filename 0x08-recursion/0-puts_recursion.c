25d95c681398805c1fe24bbbd26fd58cfbaaf5c0
ronkola2003 <oyebeautypalace@gmail.com>
2023-09-28 11:54:55 -0700

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,15 +3,15 @@
 /**
  * _puts_recursion - prints s string
  * @s: string to print
- * Return: 0
+ * Return: Always 0
  */
 void _puts_recursion(char *s);
 {
-	if (s)
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
-	}
+	} 
 	else
 	_putchar("\n");
 }
