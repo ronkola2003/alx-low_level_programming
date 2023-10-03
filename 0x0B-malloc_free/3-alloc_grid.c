acb835895087997491e4ed88855f49b77b603110
ronkola2003 <oyebeautypalace@gmail.com>
2023-10-03 11:12:37 -0700

    3-allo_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,20 +14,24 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
+
 	mee = malloc(sizeof(int *) * height);
+
 	if (mee == NULL)
 		return (NULL);
+
 	for (a = 0; a < height; a++)
 	{
-		mee[a] = malloc(sizeof(int) * width)
+		mee[a] = malloc(sizeof(int) * width);
 			if (mee[a] == NULL)
+			{
+				for (; a >= 0; a--)
+					free(mee[a]);
+				free(mee);
+				return (NULL);
+			}
 	}
-	for (; a >= 0; a--)
-		free(mee[a]);
-	free(mee);
-	return (NULL);
-}	
-	}
+
 	for (a = 0; a < height; a++)
 	{
 
