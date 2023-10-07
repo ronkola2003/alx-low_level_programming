8ab14eb39775154bbe851065537f6998fc47529d
ronkola2003 <oyebeautypalace@gmail.com>
2023-10-07 07:57:40 -0700

    main.h

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stlib.h>
+#include <limits.h>
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int_putchar(char c);
+int_strlen(char *s);
+int is_digit(chat *s);
+
+#endif
