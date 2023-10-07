aead515b6dc4246d28324611d1375263f57c189a
ronkola2003 <oyebeautypalace@gmail.com>
2023-10-07 10:17:04 -0700

    Trust no one

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stlib.h>
 #include <limits.h>
 
 void *malloc_checked(unsigned int b);
@@ -12,6 +11,6 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 int _strlen(char *s);
-int is_digit(chat *s);
+int is_digit(char *s);
 
 #endif
