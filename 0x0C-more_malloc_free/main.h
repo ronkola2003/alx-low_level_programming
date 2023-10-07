eac38103f5eedd2308295326ddee333854770e86
ronkola2003 <oyebeautypalace@gmail.com>
2023-10-07 08:40:01 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,8 +10,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int_putchar(char c);
-int_strlen(char *s);
+int _putchar(char c);
+int _strlen(char *s);
 int is_digit(chat *s);
 
 #endif
